26638aec7044d69dbf4ae42a46a9481b5460b424
zoud <zoud@rose-hulman.edu>
2019-04-02 16:21:23 -0400

    DONE

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -11,16 +11,16 @@ for ITERATING through SEQUENCES, including selections from:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Deng Zou.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
     """ Calls the   TEST   functions in this module. """
-    run_test_shortest_string()
-    run_test_index_of_largest_number()
-    run_test_number_of_stutters()
-    run_test_is_palindrome()
+    # run_test_shortest_string()
+    # run_test_index_of_largest_number()
+    # run_test_number_of_stutters()
+    # run_test_is_palindrome()
     run_test_count_same()
 
 
@@ -104,10 +104,15 @@ def shortest_string(strings):
       :type strings: list[str]   or tuple(str)
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     # -------------------------------------------------------------------------
+    index_of_min = 0
+    for k in range(len(strings)):
+        if len(strings[k]) < len(strings[index_of_min]):
+            index_of_min = k
 
+    return strings[index_of_min]
 
 def run_test_index_of_largest_number():
     """ Tests the   index_of_largest_number   function. """
@@ -181,10 +186,14 @@ def index_of_largest_number(numbers, n):
 
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     # -------------------------------------------------------------------------
-
+    max=0
+    for k in range(n):
+        if numbers[k] > numbers[max]:
+            max = k
+    return max
 
 # -----------------------------------------------------------------------------
 # Some problems iterate (loop) through the sequence accessing TWO
@@ -236,10 +245,14 @@ def number_of_stutters(s):
        :type s: str
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     # -------------------------------------------------------------------------
-
+    count=0
+    for k in range (len(s)-1):
+        if s[k+1]==s[k]:
+            count=count+1
+    return count
 
 def run_test_is_palindrome():
     """ Tests the   is_palindrome   function. """
@@ -309,7 +322,7 @@ def is_palindrome(s):
       :type s: str
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     ###########################################################################
@@ -318,6 +331,10 @@ def is_palindrome(s):
     #   above are particularly good examples to work by hand.
     ###########################################################################
     # -------------------------------------------------------------------------
+    for k in range (len(s)//2):
+        if s[k]!= s[len(s)-1-k]:
+            return False
+    return True
 
 
 # -----------------------------------------------------------------------------
@@ -376,10 +393,14 @@ def count_same(sequence1, sequence2):
       type: sequence2: tuple or list or string
     """
     # -------------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #     The testing code is already written for you (above).
     # -------------------------------------------------------------------------
-
+    count=0
+    for k in range (len(sequence1)):
+        if sequence1[k]==sequence2[k]:
+            count=count+1
+    return count
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
